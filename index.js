210362872aee3ac77999ac83de7bd753d558cdac
Marcio <marcio@Stefans-Air.fritz.box>
2023-04-25 12:06:24 +0200

    bootstrap

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express()
 
 //HTML render EJS
 app.set(`view engine`, `ejs`);
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
     res.render("index")
 });
+
+app.get("/questions", (req, res) => {
+    res.render("questions")
+})
 
 app.listen(8080, () => {
     console.log("Running on PORT 8080")
