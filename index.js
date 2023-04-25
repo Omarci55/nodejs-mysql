04249144335a204359f7ff04ed3a6639c0de3ccd
Marcio <marcio@Stefans-MacBook-Air.local>
2023-04-25 17:24:45 +0200

    sequelize

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const app = express()
+const bodyParser = require("body-parser");
+const connection = require("./database/database")
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Connected with Database")
+    })
+    .catch( (err) => {
+        console.log(err)
+    })
 
 //HTML render EJS
 app.set(`view engine`, `ejs`);
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//routes
 app.get("/", (req, res) => {
     res.render("index")
 });
 
 app.get("/questions", (req, res) => {
     res.render("questions")
+});
+
+app.post("/saving", (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    res.send("Form received! Title " + title + " " + "description " + description);
 })
 
 app.listen(8080, () => {
